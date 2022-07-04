4f3ac3e0b2147da107dd43ff32bfb29e49833dbe
jahidshakil <jahidaladin@gmail.com>
2022-07-04 23:35:33 +0600

    bestSum

---
+++ bestSum.js
@@ -0,0 +1,25 @@
+const bestSum = (targetSum, nums, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+    let shortestCombination = null;
+
+    for (let num of nums) {
+        const remainder = targetSum - num;
+        const remainderCombination = bestSum(remainder, nums);
+        if (remainderCombination !== null) {
+            const combination = [...remainderCombination, num];
+            if (shortestCombination===null || combination.length < shortestCombination.length) {
+                shortestCombination = combination;
+            }
+        }
+    }
+    memo[targetSum] = shortestCombination;
+    return shortestCombination;
+}
+
+
+console.log(bestSum(7, [5, 3, 4, 7]))
+console.log(bestSum(11, [5, 3, 4, 7]))
+console.log(bestSum(101,[5,3,4,7]))
+
