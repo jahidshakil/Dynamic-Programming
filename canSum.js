e8316995ffee427527697279257a673dff89eb86
jahidshakil <jahidaladin@gmail.com>
2022-08-17 13:52:33 +0600

    edited cansum

--- canSum.js
+++ canSum.js
@@ -2,6 +2,8 @@
 //(7, [5, 3, 4, 7]) true
 // (7,[2,6,4]) false
 
+
+// Optimal solution
 const canSum = (targetSum, numbers, memo = {}) => {
 
     if (targetSum in memo) return memo[targetSum];
@@ -14,12 +16,25 @@ const canSum = (targetSum, numbers, memo = {}) => {
         if (canSum(remainder, numbers, memo) === true) {
             memo[targetSum] = true;
             return true;
-        }  
+        }
     }
     memo[targetSum] = false;
     return false;
     
 }
+
+
+// recursive not optimal solution
+
+// const canSum = (target, numbers) => {
+//     if (target === 0) return true;
+//     if(target<0) return false
+//     for (let i of numbers) {
+//         const remainder = target - i;
+//         if(canSum(remainder,numbers)===true) return true
+//     }
+//     return false
+// }
 
 console.log(canSum(7, [2, 3])) //true
 console.log(canSum(7, [5,7, 3])) //true
@@ -27,4 +42,3 @@ console.log(canSum(7, [2, 4])) //false
 console.log(canSum(8, [2, 3,5])) //true
 console.log(canSum(300, [7, 14])) //false
 console.log(canSum(30000, [7, 14])) //false
-
